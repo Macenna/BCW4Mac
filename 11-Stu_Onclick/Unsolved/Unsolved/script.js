df1d0a52bd7cf0681b1e6d78e5a00501926eeb5c
Macenna <macennacowen@gwu.edu>
2020-11-21 11:37:58 -0500

    11 - notes & working on

--- 11-Stu_Onclick/Unsolved/Unsolved/script.js
+++ 11-Stu_Onclick/Unsolved/Unsolved/script.js
@@ -1,4 +1,19 @@
-/*
+//start by listing the vars necessary
 var count = 0;
-var incrementEl = document.querySelector()
-*/ 
+var incrementEl = document.querySelector("#increment"); //var linked to an element 
+var decrementEl = document.querySelector("#decrement"); // ditto^
+var countEl = document.querySelector("#count");
+
+// console.log the 3 elements (not including the count) 
+console.log(incrementEl);
+console.log(decrementEl);
+console.log(countEl);
+
+// add function statement for the counter + its text
+function setCounterText(params) {
+    countEl.textContent = count;    
+}
+
+// event listener with a callback that increments the counter
+
+// displays the updated count on the webpage
