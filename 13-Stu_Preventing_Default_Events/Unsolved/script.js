2fe695c2ae7eb190b6c11c6222604aa32531a97b
Macenna <macennacowen@gwu.edu>
2020-11-21 13:10:16 -0500

    13 - JS notes

---
+++ 13-Stu_Preventing_Default_Events/Unsolved/script.js
@@ -0,0 +1,27 @@
+// Set up the variables
+var mealCost = document.querySelector("#form")
+
+// function to calculate tip w/ params total, tip % 
+    // var w/ formula to calculate tip (% of total meal amount)
+    // return result
+
+// function to calculate total w/ tip amount 
+    // return total + tip 
+        // parseFloat = makes integers usable
+
+// function to add tip 
+    // bring values from vars at the top to display values on webpage 
+    // event function calling prevent Default 
+    // 4 variables, bottom 2 with functions 
+    // 2 query selectors, tip amount & new total 
+
+
+// function to split the total 
+
+
+
+
+// 2 event listeners - add tip and split total on-click functions at the bottom
+        // these 2 event listeners call the outputs from the functions above 
+    //submitEl.addEventListener("click", addTip);
+    //splitEl.addEventListener("click", splitTotal);
