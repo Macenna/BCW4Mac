f85872d4f721c3fbbefad40ababae6be399641b5
Macenna <macennacowen@gwu.edu>
2020-12-01 15:44:33 -0500

    9 - Complete & Notes

--- 11-Stu_Onclick/Unsolved/Unsolved/script.js
+++ 11-Stu_Onclick/Unsolved/Unsolved/script.js
@@ -1,29 +1,40 @@
 //start by listing the vars necessary
 var count = 0;
-var incrementEl = document.querySelector("#increment"); //var linked to an element 
+var incrementEl = document.querySelector("#increment"); // var linked to a button element by ID name 
 var decrementEl = document.querySelector("#decrement"); // ditto^
-var countEl = document.querySelector("#count");
+var countEl = document.querySelector("#count");         // Count element variable values counter element of application
 
-// console.log the 3 elements (not including the count) 
+
+// console.log the 3 elements (not including the counter itself but yes the element the counter is in) 
+    // Displays the updated count to the console 
 console.log(incrementEl);
 console.log(decrementEl);
 console.log(countEl);
 
+
 // add function statement for the counter + its text
-function setCounterText(params) {
+function setCounterText() {
     countEl.textContent = count;    
-}
+};
+
 
 // event listener with a callback that increments the counter
     // Have to add at least 2 parameters in addEventListener when calling each increment 
-        // Start w/ element, call an event listener, set click as the first parameter, then a function as the second parameter of the call
+        // Start w/ element, call an event listener, set click as the first parameter (the event), then a function as the second parameter to be called
             // Then, inside function, add a count, ++ -> count upwards 1 integer each counter
                 // then, inside function one line down, set the counter text
-incrementEl.addEventListener ("click", function name(params) {
-    
-})
+incrementEl.addEventListener ("click", function () {
+    count++;
+    setCounterText();       // This calls the function above which updates the counter text
+});
+
 
 // Add second event listener, but add an if statement b/c it is not the first function called 
+decrementEl.addEventListener("click", function() {
+    if(count > 0) {
+        count--;
+        setCounterText();
+    }
+});
 
-
-// displays the updated count on the webpage
+// Displays the updated count on the webpage
