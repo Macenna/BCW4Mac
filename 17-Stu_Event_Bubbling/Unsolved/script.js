3cbf6cd86227c191f9357f78f46fe10136c3c807
Macenna <macennacowen@gwu.edu>
2020-12-03 16:08:51 -0500

    17 - Working on

---
+++ 17-Stu_Event_Bubbling/Unsolved/script.js
@@ -0,0 +1,22 @@
+// Global elements being manipulated for carousel functionality 
+var carousel = getElementByClass(".carouselbox");       // This is the element constantly being manipulated 
+var prevBtn = getElementByClass(".prev");
+var nextBtn = getElementByClass(".next");
+
+var images = [
+    "https://picsum.photos/300/200",
+    "https://picsum.photos/300/201",
+    "https://picsum.photos/300/202",
+    "https://picsum.photos/300/203"
+];
+
+var index = 0; 
+
+
+
+//function navNext () {
+//   carousel.
+//}
+
+//nextBtn.carousel("next");
+//prevBtn.carousel("prev");
