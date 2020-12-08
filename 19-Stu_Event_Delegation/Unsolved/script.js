b19bb0148af71c68bcaad7e7b0848ad1d904ac97
Macenna <macennacowen@gwu.edu>
2020-12-08 17:59:37 -0500

    19 - Worked on

--- 19-Stu_Event_Delegation/Unsolved/script.js
+++ 19-Stu_Event_Delegation/Unsolved/script.js
@@ -24,9 +24,28 @@ function close() {
   modalEl.style.display = "none";
 }
 
-function handleClick() {
+function handleClick(event) {
+  if(event.target.matches("button")) {
+    event.preventDefault();
+    modalEl.style.display = "block";                          // Style the modal container w/ block elements 
+    currentId = parseInt(event.target.parentElement.id);      // Change the ID of the parent element to the next index of the child 
+    var name = people[currentId].name;                        // Add to the people array in the name property/element
+    var description = people[currentId].description;          // Add to the people array in the description property 
+    modalNameEl.textContent = name;                           // The text content inside the add person query is equal to a name value 
+    if(deescription) {
+      descriptionEl.value = description;                      // value of local var deescription is = to description element property for that value 
+    }
+    else {
+      descriptionEl.value = "";
+    }
+  };
   // Use event delegation to handle when the user clicks "edit"
-}
+};
 
 closeEl.addEventListener("click", close);
+saveBtn.addEventListener("click", function (event){
+  event.preventDefault();
+  people[currentId].description = descriptionEl.value;
+  close();
+});
 addBtn.addEventListener("click", addPersonToList);
